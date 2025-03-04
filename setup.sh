fed9fe8c30c5379b2f69fc3be0455dd07617b63b
Trieu Vu <trieuv@logicly.com.au>
2025-03-04 16:55:26 +1100

    rework setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 echo "Setup profile"
-cp profile ~/.profile
-source ~/.profile
+cp profile $HOME/.profile
+source $HOME/.profile
 
 echo "Setup vimrc"
-cp vimrc ~/.vimrc
+cp vimrc $HOME/.vimrc
+
+echo "Setup nvim config"
+mkdir -p $HOME/.config/nvim/
+cp init.vim $HOME/.config/nvim/
 
 echo "Setup vim plugin"
-mkdir -p ~/.vim/
-cp -r vim/ ~/.vim/
+mkdir -p $HOME/.vim/
+cp -r vim/ $HOME/.vim/
+echo "Download vim plug"
+mkdir -p $HOME/.vim/autoload/
+curl -Ls https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > $HOME/.vim/autoload/plug.vim
 
 echo "Open .vimrc and run :PlugInstall to download all plugins"
 
